d17bc19036389b95689ed9d347aef11816b1e331
cmllnnmrc <cuetolovely0118@gmail.com>
2023-05-10 15:45:56 +0800

    Spotify Responsive

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -6,3 +6,23 @@ toggleMenu.addEventListener("click", () => {
   headNav.classList.toggle("open");
   headBlur.classList.toggle("open");
 });
+
+const links = document.querySelectorAll(".links__drop__item h4");
+links.forEach((down) => {
+  down.addEventListener("click", () => {
+    down.nextElementSibling.classList.toggle("open");
+    down.querySelector("i").classList.toggle("open");
+  });
+});
+
+// const brand__img = document.querySelector(".branding img");
+// const header = document.querySelector(".header");
+// window.addEventListener("scroll", () => {
+//   console.log(window.pageYOffset);
+//   if (window.pageYOffset >= 100) {
+//     header.classList.add("blue");
+//     brand__img.style="background: black; border-radius: 5rem";
+//   } else {
+//     header.classList.remove("blue");
+//   }
+// });
